0061f7c333ee73145b1c2f61574075d896ce7397
chika3742 <kazu.chika.shima@gmail.com>
2024-02-13 22:54:51 +0900

    fix: RiverpodのkeepAliveをtrueに
    ページを移動してもステートが保持されるように

--- lib/core/versions.dart
+++ lib/core/versions.dart
@@ -3,7 +3,7 @@ import "package:riverpod_annotation/riverpod_annotation.dart";
 
 part "versions.g.dart";
 
-@riverpod
+@Riverpod(keepAlive: true)
 Future<PackageInfo> packageInfo(PackageInfoRef ref) async {
   return await PackageInfo.fromPlatform();
 }
--- lib/core/versions.g.dart
+++ lib/core/versions.g.dart
@@ -6,11 +6,11 @@ part of 'versions.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$packageInfoHash() => r'70b4a9f2a022e5b2378e51d1d49d1f88448f9784';
+String _$packageInfoHash() => r'9fe479523e8271f8a97e79dcdc75029c05d9c483';
 
 /// See also [packageInfo].
 @ProviderFor(packageInfo)
-final packageInfoProvider = AutoDisposeFutureProvider<PackageInfo>.internal(
+final packageInfoProvider = FutureProvider<PackageInfo>.internal(
   packageInfo,
   name: r'packageInfoProvider',
   debugGetCreateSourceHash:
@@ -19,6 +19,6 @@ final packageInfoProvider = AutoDisposeFutureProvider<PackageInfo>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef PackageInfoRef = AutoDisposeFutureProviderRef<PackageInfo>;
+typedef PackageInfoRef = FutureProviderRef<PackageInfo>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
