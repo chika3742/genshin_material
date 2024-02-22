1fd16b7aec8f346cb80cb0048eb703798626e7f3
chika3742 <kazu.chika.shima@gmail.com>
2024-02-22 22:56:08 +0900

    feat: 再ダウンロード時にモーダルを表示するように
    アセットダウンロード開始前にページ移動されることを阻止

---
+++ lib/utils/show_loading_modal.dart
@@ -0,0 +1,18 @@
+import "package:animations/animations.dart";
+import "package:flutter/material.dart";
+
+void showLoadingModal(BuildContext context) {
+  showModal(
+    context: context,
+    builder: (context) {
+      return PopScope(
+        canPop: false,
+        child: Center(
+          child: CircularProgressIndicator(
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+        ),
+      );
+    },
+  );
+}
