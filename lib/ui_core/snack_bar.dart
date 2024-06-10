25cdc429db37711ef1f4525c246484b1623d37dd
chika3742 <kazu.chika.shima@gmail.com>
2024-06-11 00:14:59 +0900

    feat: エラー時にSnackbarの表示時間を長く

--- lib/ui_core/snack_bar.dart
+++ lib/ui_core/snack_bar.dart
@@ -3,9 +3,10 @@ import "package:flutter/material.dart";
 void showSnackBar({
   required BuildContext context,
   required String message,
-  Duration duration = const Duration(seconds: 4),
+  Duration? duration,
   bool error = false,
 }) {
+  duration ??= error ? const Duration(seconds: 10) : const Duration(seconds: 4);
   final messenger = ScaffoldMessenger.of(context);
   messenger.hideCurrentSnackBar();
   messenger.showSnackBar(
