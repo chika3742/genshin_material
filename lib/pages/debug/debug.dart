1b9614b90d0175748af89d8e196dca76221feeae
chika3742 <kazu.chika.shima@gmail.com>
2024-07-07 01:19:49 +0900

    feat: デバッグメニューにアセット更新の項目を追加

--- lib/pages/debug/debug.dart
+++ lib/pages/debug/debug.dart
@@ -1,15 +1,22 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:path_provider/path_provider.dart";
 
 import "../../components/list_tile.dart";
+import "../../core/asset_updater.dart";
 import "../../i18n/strings.g.dart";
 import "../../main.dart";
+import "../../providers/asset_updating_state.dart";
+import "../../providers/versions.dart";
 import "../../routes.dart";
+import "../../ui_core/install_latest_assets.dart";
+import "../../ui_core/snack_bar.dart";
 
-class DebugMenuPage extends StatelessWidget {
+class DebugMenuPage extends ConsumerWidget {
   const DebugMenuPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Debug Menu"),
@@ -49,6 +56,23 @@ class DebugMenuPage extends StatelessWidget {
                 },
               ),
             ],
+          ),
+          SimpleListTile(
+            title: "Check for Asset Update",
+            enabled: ref.watch(assetUpdatingStateNotifierProvider).state == null,
+            onTap: () async {
+              final assetDir = ref.read(assetDataProvider).value!.assetDir;
+              final tempDir = await getTemporaryDirectory();
+              final updater = AssetUpdater(assetDir, tempDir: tempDir.path);
+              await updater.checkForUpdate();
+              if (context.mounted) {
+                if (updater.isUpdateAvailable) {
+                  await installLatestAssets(context: context, ref: ref, updater: updater);
+                } else {
+                  showSnackBar(context: context, message: "No updates found.");
+                }
+              }
+            },
           ),
         ],
       ),
