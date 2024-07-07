771b4a6c8392450ad7f6606245ce25638190f7d0
chika3742 <kazu.chika.shima@gmail.com>
2024-07-07 19:28:49 +0900

    fix: ブックマークボタンを押しても何も起こらない場合がある問題の修正

--- lib/components/material_item.dart
+++ lib/components/material_item.dart
@@ -1,3 +1,4 @@
+import "package:collection/collection.dart";
 import "package:flutter/material.dart" hide Material;
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
@@ -43,6 +44,12 @@ class _MaterialItemState extends ConsumerState<MaterialItem> {
               purposeTypes: widget.possiblePurposeTypes,
               bookmarkType: widget.usage.type,
             ),
+        [
+          widget.usage.characterId,
+          widget.item.id,
+          const ListEquality().hash(widget.possiblePurposeTypes),
+          widget.usage.type,
+        ],
       ),
     );
 
