9f65800707fa54df5f6ab498bbedd7dceb71d1a7
chika3742 <kazu.chika.shima@gmail.com>
2024-08-24 23:37:29 +0900

    アセット配信用ドメイン変更

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -2,6 +2,6 @@ const tosUrl = "https://gms.chikach.net/terms";
 
 const privacyPolicyUrl = "https://gms.chikach.net/privacy";
 
-const assetReleasesUrl = "https://asset-releases.matnote.app/releases/genshin";
+const assetReleasesUrl = "https://matnote-releases.chikach.net/releases/genshin";
 
 const wishesPageUrl = "https://gs.matnote.app/wishes";
