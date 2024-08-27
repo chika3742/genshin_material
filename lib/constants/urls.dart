20ee2c7668c67f0a93406cbd80a12450fb539c3d
chika3742 <kazu.chika.shima@gmail.com>
2024-08-27 13:59:04 +0900

    feat: 素材の入手先表示追加

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -5,3 +5,5 @@ const privacyPolicyUrl = "https://gms.chikach.net/privacy";
 const assetReleasesUrl = "https://matnote-releases.chikach.net/releases/genshin";
 
 const wishesPageUrl = "https://gs.matnote.app/wishes";
+
+const teyvatMapUrl = "https://act.hoyolab.com/ys/app/interactive-map/#/map/2";
