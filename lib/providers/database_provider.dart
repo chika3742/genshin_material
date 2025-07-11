f36fb7dcfef45fde4991e8053962149c60ad8c8a
chika3742 <kazu.chika.shima@gmail.com>
2025-07-11 22:41:26 +0900

    refactor: remove unused provider

--- lib/providers/database_provider.dart
+++ lib/providers/database_provider.dart
@@ -4,9 +4,7 @@ import "package:riverpod_annotation/riverpod_annotation.dart";
 import "../database.dart";
 import "../db/bookmark_db_extension.dart";
 import "../db/bookmark_order_registry_db_extension.dart";
-import "../db/material_bag_count_db_extension.dart";
 import "../models/bookmark.dart";
-import "preferences.dart";
 
 part "database_provider.g.dart";
 
@@ -40,14 +38,4 @@ Stream<List<BookmarkWithDetails>> bookmarks(Ref ref, {String? groupHash, List<St
 Stream<List<String>> bookmarkOrder(Ref ref) {
   final db = ref.watch(appDatabaseProvider);
   return db.watchBookmarkOrder();
-}
-
-@riverpod
-Stream<int?> bagCount(Ref ref, int hyvId) async* {
-  final db = ref.watch(appDatabaseProvider);
-  final prefs = ref.watch(preferencesStateNotifierProvider);
-  if (!prefs.isLinkedWithHoyolab) {
-    yield null;
-  }
-  yield* db.watchMaterialBagCounts(prefs.hyvUid!, [hyvId]).map((e) => e.firstOrNull?.count);
 }
