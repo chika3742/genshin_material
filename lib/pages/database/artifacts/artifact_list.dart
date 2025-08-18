b748ecdd417f8944b61aa8c3dc383e99912d1e0c
chika3742 <kazu.chika.shima@gmail.com>
2025-08-18 22:26:47 +0900

    Add search functionality for artifacts

--- lib/pages/database/artifacts/artifact_list.dart
+++ lib/pages/database/artifacts/artifact_list.dart
@@ -1,8 +1,10 @@
+import "package:collection/collection.dart";
 import "package:flutter/material.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:material_symbols_icons/material_symbols_icons.dart";
 
 import "../../../components/list_tile.dart";
+import "../../../components/search.dart";
 import "../../../core/asset_cache.dart";
 import "../../../i18n/strings.g.dart";
 import "../../../models/artifact.dart";
@@ -40,6 +42,50 @@ class ArtifactListPage extends HookWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(appBarTitle),
+        actions: [
+          SearchButton(
+            hintTargetText: tr.pages.artifacts,
+            queryCallback: (query) {
+              return filterBySearchQuery(<ArtifactSetOrPiece>[
+                ...assetData.artifactSets.values,
+                ...assetData.artifactPieces.values,
+              ], query).sorted((a, b) {
+                if ((a is ArtifactSet && b is ArtifactSet)
+                    || (a is ArtifactPiece && b is ArtifactPiece)) {
+                  return 0;
+                } else if (a is ArtifactSet) {
+                  return -1; // Sets come before pieces
+                } else {
+                  return 1; // Pieces come after sets
+                }
+              });
+            },
+            resultItemBuilder: (context, item) {
+              final image = switch (item) {
+                ArtifactSet(:final getFirstPiece) =>
+                    getFirstPiece(assetData).getImageFile(assetData.assetDir),
+                ArtifactPiece(:final getImageFile) =>
+                    getImageFile(assetData.assetDir),
+              };
+              return SearchResultListTile(
+                image: Image.file(
+                  image,
+                  width: 40,
+                  height: 40,
+                ),
+                title: item.name.localized,
+                subtitle: switch (item) {
+                  ArtifactSet() => tr.search.targets.artifactSet,
+                  ArtifactPiece() => tr.search.targets.artifactPiece,
+                },
+                location: ArtifactDetailsRoute(id: switch (item) {
+                  ArtifactSet(:final id) => id,
+                  ArtifactPiece(:final id) => assetData.artifactPieces[id]!.parentId,
+                }).location,
+              );
+            },
+          ),
+        ],
       ),
       body: CustomScrollView(
         slivers: [
