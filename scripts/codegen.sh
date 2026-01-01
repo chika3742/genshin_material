f6f752e537f3a1806e4645248e2498145ad5c27c
chika3742 <kazu.chika.shima@gmail.com>
2026-01-01 23:12:39 +0900

    fix codegen script

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-fvm dart run build_runner watch --delete-conflicting-outputs
-fvm dart run slang watch
+fvm dart run build_runner build --delete-conflicting-outputs
+fvm dart run slang
