16445908c2c01da08ab5c14d2a6338dea1ac1a65
chika3742 <kazu.chika.shima@gmail.com>
2026-01-06 20:24:21 +0900

    Update wishes page url

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -4,7 +4,7 @@ const privacyPolicyUrl = "https://gms.chikach.net/privacy";
 
 const assetReleasesUrl = "https://matnote-releases.chikach.net/releases/genshin";
 
-const wishesPageUrl = "https://gs.matnote.app/wishes";
+const wishesPageUrl = "https://gc.chikach.net";
 
 const teyvatMapUrl = "https://act.hoyolab.com/ys/app/interactive-map/#/map/2";
 
