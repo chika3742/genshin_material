cf263b461b2a400edfd4f970f95051abd7f22e1f
chika3742 <kazu.chika.shima@gmail.com>
2026-05-02 19:32:20 +0900

    chore(ios): Migrate to UIScene (closes #372)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,19 +2,10 @@ import UIKit
 import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    guard let controller = window?.rootViewController as? FlutterViewController else {
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+    func didInitializeImplicitFlutterEngine(_ engineBridge: any FlutterImplicitEngineBridge) {
+        GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+        
+        HoyolabIntegrationApiSetup.setUp(binaryMessenger: engineBridge.applicationRegistrar.messenger(), api: HoyolabIntegrationApiImplementation())
     }
-    
-    HoyolabIntegrationApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: HoyolabIntegrationApiImplementation())
-      
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
