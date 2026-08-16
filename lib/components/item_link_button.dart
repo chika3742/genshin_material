4ba77843a67c01dabdddc20d539ddeed2d4f9911
chika3742 <kazu.chika.shima@gmail.com>
2026-08-16 16:29:05 +0900

    Adjustments for artifact effect list

--- lib/components/item_link_button.dart
+++ lib/components/item_link_button.dart
@@ -1,10 +1,16 @@
 import "package:flutter/material.dart";
 
 class ItemLinkButton extends StatelessWidget {
+  final EdgeInsetsGeometry padding;
   final GestureTapCallback? onTap;
   final Widget? child;
 
-  const ItemLinkButton({super.key, this.onTap, this.child});
+  const ItemLinkButton({
+    super.key,
+    this.onTap,
+    this.child,
+    this.padding = const EdgeInsets.all(8),
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +18,7 @@ class ItemLinkButton extends StatelessWidget {
       borderRadius: BorderRadius.circular(8),
       onTap: onTap,
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: padding,
         child: child,
       ),
     );
--- lib/pages/database/artifacts/artifact_effect_list.dart
+++ lib/pages/database/artifacts/artifact_effect_list.dart
@@ -8,10 +8,13 @@ import "../../../components/chips.dart";
 import "../../../components/data_asset_scope.dart";
 import "../../../components/effect_description.dart";
 import "../../../components/filter_bottom_sheet.dart";
+import "../../../components/horizontal_chip_list.dart";
+import "../../../components/item_link_button.dart";
 import "../../../core/asset_cache.dart";
 import "../../../i18n/strings.g.dart";
 import "../../../models/artifact.dart";
 import "../../../providers/filter_state.dart";
+import "../../../routes.dart";
 
 class ArtifactEffectListPage extends HookConsumerWidget {
   final AssetData assetData;
@@ -43,100 +46,101 @@ class ArtifactEffectListPage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(tr.artifactsPage.effectList),
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 64),
+          child: SizedBox(
+            width: double.infinity,
+            child: HorizontalChipList(
+              chips: [
+                FilterChipWithMenu(
+                  label: Text(tr.artifactsPage.kindOfEffect),
+                  selected: filterState.tags.isNotEmpty,
+                  onSelected: (_) {
+                    _showFilterBottomSheet(context);
+                  },
+                ),
+                FilterChipWithIcon(
+                  leading: const Icon(Symbols.clear),
+                  label: Text(tr.common.clear),
+                  onSelected: filterState.tags.isNotEmpty ? (_) {
+                    ref.read(artifactFilterStateProvider.notifier).clear();
+                  } : null,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       body: HookBuilder(
         builder: (context) {
           final sets = useMemoized(() => _filterAndSortSets(filterState.tags), [filterState]);
           final flattenedTags = useMemoized(_flattenTags, [assetData]);
 
-          return CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: Row(
-                    spacing: 8.0,
-                    children: [
-                      FilterChipWithMenu(
-                        label: Text(tr.artifactsPage.kindOfEffect),
-                        selected: filterState.tags.isNotEmpty,
-                        onSelected: (_) {
-                          _showFilterBottomSheet(context);
-                        },
-                      ),
-                      FilterChipWithIcon(
-                        leading: const Icon(Symbols.clear),
-                        label: Text(tr.common.clear),
-                        onSelected: filterState.tags.isNotEmpty ? (_) {
-                          ref.read(artifactFilterStateProvider.notifier).clear();
-                        } : null,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SliverList.builder(
-                itemCount: sets.length,
-                itemBuilder: (context, index) {
-                  final set = sets[index];
+          return ListView.builder(
+            itemCount: sets.length,
+            itemBuilder: (context, index) {
+              final set = sets[index];
 
-                  return Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              spacing: 8.0,
-                              children: [
-                                Image.file(set.getFirstPiece(assetData).getImageFile(assetData.assetDir), width: 35, height: 35),
-                                Text(set.name.localized),
-                              ],
-                            ),
-                            SizedBox(height: 4),
-                            if (set.tags case final tags?)
-                              Wrap(
-                                spacing: 4,
-                                runSpacing: 4,
-                                children: tags.map((tag) => Chip(
-                                  label: Text(flattenedTags[tag]!.desc.localized),
-                                  shape: RoundedRectangleBorder(borderRadius: .circular(32)),
-                                  padding: .symmetric(horizontal: 4),
-                                  materialTapTargetSize: .shrinkWrap,
-                                  backgroundColor: filterState.tags.contains(tag)
-                                      ? Theme.of(context).colorScheme.primaryContainer
-                                      : null,
-                                  side: filterState.tags.contains(tag)
-                                      ? BorderSide.none
-                                      : null,
-                                )).toList(),
-                              ),
-                            for (final bonus in set.bonuses) ...[
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
-                                child: Text(
-                                  tr.artifactsPage.bonusTypes[bonus.type]!,
-                                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                                    color: Theme.of(context).colorScheme.secondary,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 8.0),
-                                child: EffectDescription(bonus.description.localized),
-                              ),
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ItemLinkButton(
+                          padding: .all(4),
+                          onTap: () {
+                            ArtifactDetailsRoute(id: set.id).push(context);
+                          },
+                          child: Row(
+                            spacing: 8.0,
+                            children: [
+                              Image.file(set.getFirstPiece(assetData).getImageFile(assetData.assetDir), width: 35, height: 35),
+                              Text(set.name.localized),
                             ],
-                          ],
+                          ),
                         ),
-                      ),
-                      const Divider(indent: 16, endIndent: 16),
-                    ],
-                  );
-                },
-              ),
-            ],
+                        SizedBox(height: 4),
+                        if (set.tags case final tags?)
+                          Wrap(
+                            spacing: 4,
+                            runSpacing: 4,
+                            children: tags.map((tag) => Chip(
+                              label: Text(flattenedTags[tag]!.desc.localized),
+                              shape: RoundedRectangleBorder(borderRadius: .circular(32)),
+                              padding: .symmetric(horizontal: 4),
+                              materialTapTargetSize: .shrinkWrap,
+                              backgroundColor: filterState.tags.contains(tag)
+                                  ? Theme.of(context).colorScheme.primaryContainer
+                                  : null,
+                              side: filterState.tags.contains(tag)
+                                  ? BorderSide.none
+                                  : null,
+                            )).toList(),
+                          ),
+                        for (final bonus in set.bonuses) ...[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                            child: Text(
+                              tr.artifactsPage.bonusTypes[bonus.type]!,
+                              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: EffectDescription(bonus.description.localized),
+                          ),
+                        ],
+                      ],
+                    ),
+                  ),
+                  const Divider(indent: 16, endIndent: 16, height: 4),
+                ],
+              );
+            },
           );
         },
       ),
